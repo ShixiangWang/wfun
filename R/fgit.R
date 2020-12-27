543f865dcc850131c133c61af7e6176aeb9afc9f
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-12-27 16:57:54 +0800

    copy install funs from yutils

--- R/fgit.R
+++ R/fgit.R
@@ -139,3 +139,49 @@ verbose_git <- function() message("See ?remotes::install_git fore more options."
 verbose_github <- function() message("See ?remotes::install_github fore more options.")
 verbose_bioc <- function() message("See ?BiocManager::install for more options.")
 verbose_clone <- function() message("See ?git2r::clone for more options.")
+
+
+# Copy from yu utils ------------------------------------------------------
+# source: https://github.com/YuLab-SMU/yulab.utils/blob/master/R/install_zip.R
+
+##' install github package
+##'
+##' it download the zip file first and use `install_zip` to install it
+##' @title install_zip_gh
+##' @param repo github repo
+##' @param ref github branch, default is master
+##' @param args argument to build package
+##' @return NULL
+##' @export
+##' @author Guangchuang Yu
+install_zip_gh <- function(repo, ref = "master", args = "--no-build-vignettes") {
+  ## repo <- 'GuangchuangYu/nCov2019'
+  url <- paste0('https://codeload.github.com/', repo, '/zip/', ref)
+  f <- tempfile(fileext=".zip")
+  method <- "auto"
+  if (.Platform$OS.type == "windows") method <- "curl"
+  download.file(url, destfile=f, method = method)
+  install_zip(f, args=args)
+}
+
+##' install R package from zip file of source codes
+##'
+##'
+##' @title install_zip
+##' @param file zip file
+##' @param args argument to build package
+##' @return NULL
+##' @export
+##' @author Guangchuang Yu
+install_zip <- function(file, args = "--no-build-vignettes") {
+  dir <- tempfile()
+  utils::unzip(file, exdir=dir)
+  fs <- list.files(path=dir, full.names=T)
+  if (length(fs) == 1 && dir.exists(fs)) {
+    dir <- fs
+  }
+  ## dir <- paste0(dir, '/', basename(repo), '-master')
+  ## remotes::install_local(path=dir, ..., force=TRUE)
+  pkg <- pkgbuild::build(dir, args=args)
+  install.packages(pkg, repos=NULL)
+}
